94d139f94b313ee4962deff5641cb8073d53b279
TheTeaCup <lw0482352@gmail.com>
2022-07-26 18:34:00 -0400

    working on ui

---
+++ pages/app/index.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/nav";
+import {Box} from "@chakra-ui/react";
+
+export default function App_Home() {
+    return (
+        <>
+            <Navbar/>
+
+            <Box p={4}>App Home</Box>
+        </>
+    )
+}
