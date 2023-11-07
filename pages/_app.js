d00226c2b507162d7a42cc7352f985ad3af48f16
TheTeaCup <lw0482352@gmail.com>
2023-11-07 12:50:35 -0500

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 export default function App({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Script async defer data-website-id="59330be7-f6ad-4697-a9a2-6800108ae8f5"
+                    src="https://analytics.theteacup.dev/script.js"></Script>
+            <Component {...pageProps} />
+        </>
+        )
 }
